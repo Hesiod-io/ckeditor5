30882f0048e6b382e91a67df7b1a79b5f0597ae3
selmi-karim <selmi.kerim@gmail.com>
2020-11-04 12:33:56 +0100

    Internal (utils): Updated version of CKEditor 5.

--- packages/ckeditor5-utils/src/version.js
+++ packages/ckeditor5-utils/src/version.js
@@ -11,7 +11,7 @@
 
 import CKEditorError from './ckeditorerror';
 
-const version = '23.1.0';
+const version = '23.2.0';
 
 /* istanbul ignore next */
 const windowOrGlobal = typeof window === 'object' ? window : global;
